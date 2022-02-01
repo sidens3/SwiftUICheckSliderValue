b39a76c339cfe40dbf6a3778c3a4a90468661fb3
Mikhail.Zinovev <mikhail.zinovev@russianpost.ru>
2022-02-01 12:02:32 +0300

    init project

---
+++ SwiftUICheckSliderValue/SwiftUICheckSliderValueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICheckSliderValueApp.swift
+//  SwiftUICheckSliderValue
+//
+//  Created by Михаил Зиновьев on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICheckSliderValueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
